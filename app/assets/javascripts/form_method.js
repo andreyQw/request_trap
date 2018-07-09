833d927dfeb11c178b19baeffd9c4e60024d8405
andreyQw <bogdanovskiya@gmail.com>
2018-07-09 10:35:59 +0300

    fix form, redis config

--- app/assets/javascripts/form_method.js
+++ app/assets/javascripts/form_method.js
@@ -1,23 +1,19 @@
 $(function () {
+    $('#form_change').attr('method', 'PUT');
+
 
     $("#url").on("change paste keyup", function() {
         $('#form_change').attr('action', $(this).val());
         $('#show_url').text($(this).val());
-        // alert($(this).val());
     });
 
     $('#method_option').change(function() {
 
         var $option = $(this).find('option:selected');
-
         var value = $option.val();
 
         $('#form_change').attr('method', value);
-
         $('input[name=_method]').val(value);
-
-        $('input[name=authenticity_token]').val($('meta[name="csrf-token"]').attr('content'));
+        // $('input[name=authenticity_token]').val($('meta[name="csrf-token"]').attr('content'));
     });
-
-
 });
